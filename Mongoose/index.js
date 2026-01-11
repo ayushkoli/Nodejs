8d908dab948a4658a9bd4b77dc6cda875b6b5145
Ayush Koli <ayushkoli0709@gmail.com>
2026-01-11 14:13:30 +0530

    update

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,60 +1,73 @@
-const express=require("express")
-const app=express()
-const port=8000;
+const express = require("express");
+const app=express();
+const mongoose = require("mongoose");
+const port = 8000;
+
+//MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const mongoose=require("mongoose")
+
+//MONGODB CONNECTION
 mongoose
-.connect('mongodb://127.0.0.1:27017/app-2')
-.then(()=>{console.log("mongodb connected");})
-.catch((error)=>{console.log(error);
-})
+  .connect("mongodb://127.0.0.1:27017/app-2")
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-const userSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    age: {
-        type:Number
-    },
-    location: {
-        type:String
-    }
-})
+//MONGO SCHEMA
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+});
 
-const Users = mongoose.model("users",userSchema)
+//User Model to interact with mongodb
+const Users = mongoose.model("users", userSchema);
 
-app.post("/users",async(req,res)=>{
-    const user=await Users.create(req.body);
-   res.json({Message:"user created",user:user})
+//Create
+app.post("/users", async (req, res) => {
+  const user = await Users.create(req.body);
+  res.status(201).json({ Message: "user created", user: user });
+});
 
+//Read
+app.get("/users", async (req, res) => {
+  const alldata = await Users.find();
+  res.send(alldata);
+});
 
-})
-app.get("/users",async(req,res)=>{
-    const alldata= await Users.find()
-    res.send(alldata)
-})
+//Read by id
+app.get("/users/:id", async (req, res) => {
+  const user = await Users.findById(req.params.id);
+  res.json(user);
+});
 
-app.get("/users/:id",async(req,res)=>{
-    const user=await Users.findById(req.params.id)
-    res.json(user)
-})
+//Update
+app.patch("/users/:id", async (req, res) => {
+  const updateduser = await Users.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json({ message: "updated", user: updateduser });
+});
 
-app.patch("/users/:id",async(req,res)=>{
-    const updateduser=await Users.findByIdAndUpdate(req.params.id,req.body,{new:true})
-    res.json({ message: "updated", user: updateduser });
-})
+//delete
+app.delete("/users/:id", async (req, res) => {
+  const user = await Users.findByIdAndDelete(req.params.id);
+  res.json({ message: "sucess", user: user });
+});
 
-app.delete("/users/:id",async(req,res)=>{
-    const user = await Users.findByIdAndDelete(req.params.id);
-    res.json({message:"sucess",user:user})
-})
-
-
-app.listen(port,()=>{
-    console.log("Server started on port :", port);
-    
-})
-
+//start server
+app.listen(port, () => {
+  console.log("Server started on port :", port);
+});
